04440c0c9235d0490f99e1c1daefea237fd9d099
catchychoruses <catchychoruses@gmail.com>
2023-07-20 08:42:07 +0200

    gitignore fix

--- prisma/migrations/20230713080326_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Recipe" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "title" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "description" TEXT NOT NULL,
-    "ingredients" TEXT NOT NULL,
-    "instructions" TEXT NOT NULL
-);
