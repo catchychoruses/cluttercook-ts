c73e6051620d52a283e8f61cb123e6a7dec8299f
catchychoruses <catchychoruses@gmail.com>
2023-08-08 00:16:21 +0200

    deployment changes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -27,6 +27,7 @@ export const authOptions: NextAuthOptions = {
   session: {
     strategy: 'jwt',
   },
+  secret: process.env.NEXTAUTH_SECRET,
   debug: process.env.NODE_ENV === 'development',
 };
 
