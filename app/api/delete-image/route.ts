09d32a63ac0ab002806f7431d879bc6e6ebd5022
catchychoruses <catchychoruses@gmail.com>
2023-10-10 03:34:58 +0200

    hotfix

--- app/api/delete-image/route.ts
+++ app/api/delete-image/route.ts
@@ -4,7 +4,8 @@ import { NextResponse } from 'next/server';
 export async function DELETE(req: Request) {
   const { searchParams } = new URL(req.url);
   const publicId = searchParams.get('publicId');
-  const res = await deleteImage(publicId);
+  let res;
+  if (publicId) res = await deleteImage(publicId);
 
   return NextResponse.json(res);
 }
